2d4c419b174981f500716df9c8da2360b9a9aa4e
ankurdotio <prajapatiankur8349@gmail.com>
2026-07-23 22:06:59 +0530

    feat: enhance genai project with memory management and new tools

--- genai/index.ts
+++ genai/index.ts
@@ -1,50 +1,79 @@
-import { ChatMistralAI } from "@langchain/mistralai";
-import { HumanMessage, AIMessage, createAgent } from "langchain"
+import { ChatMistralAI } from "@langchain/mistralai"
+import { HumanMessage, AIMessage, tool, createAgent } from "langchain"
+import dotenv from "dotenv"
+import rl from "readline/promises"
+import * as z from "zod"
+import fs from "fs/promises"
 
-import rl from "readline/promises";
-import dotenv from "dotenv";
-dotenv.config();
 
 const readline = rl.createInterface({
     input: process.stdin,
     output: process.stdout
-});
+})
 
+dotenv.config()
 
 if (!process.env.MISTRAL_API_KEY) {
-    throw new Error("MISTRAL_API_KEY is not defined in the environment variables.");
+    throw new Error("MISTRAL_API_KEY is not set in the environment variables.")
 }
 
 const model = new ChatMistralAI({
-    apiKey: process.env.MISTRAL_API_KEY,
     model: "mistral-medium-latest",
+    apiKey: process.env.MISTRAL_API_KEY,
 })
+
+
+async function readMemoryFromFile(): Promise<string> {
+    const data = await fs.readFile("./memory.md", "utf-8")
+    return data
+}
+
+const readMemory = tool(
+    readMemoryFromFile,
+    {
+        name: "readMemory",
+        description: "Reads the memory from a file and returns it as a string.",
+        schema: z.object({}),
+    }
+)
+
+async function updateMemoryInFile({ newMemory }: { newMemory: string }): Promise<string> {
+    await fs.writeFile("./memory.md", newMemory, "utf-8")
+    return "Memory updated successfully."
+}
+
+const updateMemory = tool(
+    updateMemoryInFile,
+    {
+        name: "updateMemory",
+        description: "Updates the memory in the file with new content.",
+        schema: z.object({
+            newMemory: z.string().describe("The new memory content to be overwrite to the file."),
+        }),
+    }
+)
+
 
 const agent = createAgent({
-    model
+    model,
+    tools: [readMemory, updateMemory],
+    systemPrompt: "You are a helpful assistant that can read memory from a file and answer questions based on that memory. Use the readMemory tool to access the memory when needed. update the memory if the fact is true for weeks/months"
 })
 
-const chatHistory: (HumanMessage | AIMessage)[] = [];
-let responseText = "";
+const messages = []
 
 while (true) {
-    const prompt = await readline.question("Enter your prompt: ");
 
-    chatHistory.push(new HumanMessage(prompt));
+    const prompt = await readline.question("Enter your prompt: ")
 
-    const stream = await agent.stream({
-        messages: chatHistory,
-    },
-        { streamMode: "messages" }
-    );
+    messages.push(new HumanMessage(prompt))
 
+    const response = await agent.invoke({
+        messages
+    })
 
-    for await (const [token, metadata] of stream) {
-        process.stdout.write(token.text);
-        responseText += token.text;
-    }
+    messages.push(new AIMessage(response.messages.at(-1)?.text || ""))
 
-    chatHistory.push(new AIMessage(responseText));
-    responseText = "";
-    process.stdout.write("\n");
+    console.log("Response:", response)
+
 }
---
+++ genai/memory.md
@@ -0,0 +1,5 @@
+Rohit is a student, currently in the 3rd year of B.Tech in Computer Science and Engineering.
+
+He was working on a project related to AI for getting a job after the completion of his B.Tech. The project was a loveable clone, and it has been completed.
+
+Now, he is starting a new project focused on Docker and Kubernetes for his job.
